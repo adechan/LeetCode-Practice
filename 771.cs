e847331f70f678633c08405e46d39c0235aff929
adechan <ade.enter@yahoo.com>
2020-07-10 20:21:47 +0300

    #771

---
+++ 771.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticeProblemsCode
+{
+    /*
+    You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
+    Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+    The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+    so "a" is considered a different type of stone from "A".     */
+    public class Program
+    {
+        public static bool IsInside(string text, char toCheck)
+        {
+            foreach (char character in text)
+            {
+                if (character == toCheck)
+                    return true;
+            }
+            return false;
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            foreach (char characterS in S)
+            {
+                if (IsInside(J, characterS))
+                    count++;
+            }
+
+            return count;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(NumJewelsInStones("z", "ZZ"));
+
+        }
+
+    }
+}
