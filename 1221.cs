bea2f51c0e0e2eb3a5a3e92fa9dd583eeeca7bf9
adechan <ade.enter@yahoo.com>
2020-07-11 20:59:04 +0300

    #1221

--- 1221.cs
+++ 1221.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 namespace PracticeProblemsCode
+{
     /*
     Balanced strings are those who have equal quantity of 'L' and 'R' characters.
     Given a balanced string s split it in the maximum amount of balanced strings.
